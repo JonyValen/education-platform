797ab155538f32bd1cf3340d1efe0722f3f51762
pacbac <jclaytonc99@gmail.com>
2018-03-06 02:23:51 -0800

    Cleaned up minor HTML clutter

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,7 +1,7 @@
-var numPost = 4;
+const numPost = 4;
 
 $(document).ready(function(){
-  var homeHTML = $(".center-content").html();
+  const homeHTML = $(".center-content").html();
 
   $("#home-btn").click(function(){
     $(".center-content").css("padding", "0 0 20px 0")
@@ -11,26 +11,22 @@ $(document).ready(function(){
   });
 
   $("#trending-btn").click(function(){
-    if(!$("#trending-btn").hasClass("selected-feed-type")){
+    if(!$(this).hasClass("selected-feed-type")){
       $(".center-content").css("padding", "20px")
       $(".selected-feed-type").removeClass("selected-feed-type")
       $(this).addClass("selected-feed-type")
       $(".center-content").html("")
       /*AJAX RETRIEVE POSTS ASYNCHRONOUSLY*/
-      var i = 0
-      var requestPosts = {
+      let i = 0
+      const requestPosts = {
         url: 'post.html',
         async: true,
         type: 'get',
-        success: function(data){
-          updatePosts(data)
-        },
-        error: function(){
-          $(".center-feed").append('<div class="item">Could not load posts. Please try again later.</div>')
-        }
+        success: data => updatePosts(data),
+        error: () => $(".center-feed").append('<div class="item">Could not load posts. Please try again later.</div>')
       }
 
-      function updatePosts(data) {
+      const updatePosts = data => {
         if(i >= numPost) return
         $(".center-content").append(data)
         $(".temp .topic").text("Sample Topic "+i)
